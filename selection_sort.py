7d3c9e44f29b719a7d20182f2b9ae1b95e7b2778
Ivan Tellurian <ivantellurian@Ivans-MacBook-Air.local>
2021-10-10 19:30:58 +0300

    added .py file

---
+++ selection_sort.py
@@ -0,0 +1,25 @@
+import shutil
+columns = shutil.get_terminal_size().columns
+
+#smallest drop!
+
+def Selection():
+	for i in range(n-1):
+		minimum = i
+		for j in range(i+1, n):
+			if array[minimum] > array[j]:
+				minimum = j
+		array[i],array[minimum] = array[minimum],array[i]
+
+	return array
+
+if __name__=="__main__":
+	print("SELECTION SORT".center(columns))
+	array = input("\nInput numbers separated by commas: ").split(",")
+	array = [int(x) for x in array]
+
+	n = len(array)
+	print(Selection())
+
+
+
